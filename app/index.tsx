01a05f081f85b7660bb4730863c7c171fcadfdfe
Sample-02 <02sample.esp@gmail.com>
2026-02-23 17:28:33 +0100

    Nativewind & Tailwindcss - OK

--- app/index.tsx
+++ app/index.tsx
@@ -2,8 +2,8 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View>
-      <Text>Test NativeWind</Text>
+    <View className="flex flex-1 justify-center items-center">
+      <Text className="text-blue-500 font-bold text-3xl">Test NativeWind</Text>
     </View>
   );
 }
